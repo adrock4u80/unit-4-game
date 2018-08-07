2630f5d8526ed0a6672a328850d8647fc7bb61f3
Adam Kadivar <adkads27@gmail.com>
2018-08-07 17:52:17 -0600

    files

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+
+//random number generator for the game
+$( document ).ready(function(){
+var random = math.floor((math.random) * 102) + 19;
+$("#randomNumber").html(random);
+
+//score variables
+var wins= 0;
+var losses = 0;
+var counter = 0,
+var generatedNumber = "";
+
+//loop to generate random numbers for crystals
+for (var i=0; i < 4; i++);
+    var crystalNumber = math.floor((math.random) * 12)+1;
+    generatedNumber.push(crystalNumber);
+
+
+
+
+
+
+
+
+
+});
